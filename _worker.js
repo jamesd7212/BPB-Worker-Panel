6410142d106faf60c4a666261bc070107f3f2f0c
jamesd7212 <70996522+jamesd7212@users.noreply.github.com>
2024-09-17 14:51:56 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'f375587a-b321-4bec-9580-0b5baa976e29';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
